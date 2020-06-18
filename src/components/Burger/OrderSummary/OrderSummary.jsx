19941594276d6edb3d3ad55fae4a60aa3fda0845
sneprokin <neprokinalex94@gmail.com>
2020-06-18 21:28:36 +0300

    app optimization

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -4,10 +4,6 @@ import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-   componentDidUpdate() {
-       console.log('[OrderSummary] Did Update');
-   } 
-    
     render() {
         const ingredientSummary = Object.keys( this.props.ingredients )
         .map( igKey => {
